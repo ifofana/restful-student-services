d28ad2681782e603024a37d7b2896f010b25cfa3
ifofana <ibrahima.fofana.ia@gmail.com>
2019-08-28 12:29:38 -0400

    Added @Id

--- src/main/java/com/ia/restfulstudentservices/model/ContactInfo.java
+++ src/main/java/com/ia/restfulstudentservices/model/ContactInfo.java
@@ -1,11 +1,21 @@
 package com.ia.restfulstudentservices.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Table;
+
+import lombok.NonNull;
+
+import javax.persistence.Id;
 
 @Entity
 @Table(name = "EMERGENCY_CONTACT_INFO")
 public class ContactInfo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private @NonNull Long id;
 
 	public ContactInfo() {
 	}
