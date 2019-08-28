090862ee4f9a522929bad04a278227419de0cca0
ifofana <ibrahima.fofana.ia@gmail.com>
2019-08-28 11:49:13 -0400

    Added lombak capabilities to reduce boiler plate codes

--- src/main/java/com/ia/restfulstudentservices/model/User.java
+++ src/main/java/com/ia/restfulstudentservices/model/User.java
@@ -1,27 +1,28 @@
 package com.ia.restfulstudentservices.model;
 
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "user")
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public class User implements Serializable {
 	
 	/**
@@ -30,17 +31,17 @@ public class User implements Serializable {
 	private static final long serialVersionUID = -6147614719923616174L;
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private @NonNull Long id;
 	
 	
 	@Column(name = "name")
-	private String name;
+	private @NonNull String name;
 	
 	@Column(name = "username")
-	private String username;
+	private @NonNull String username;
 	
 	@Column(name = "password")
-	private String password;
+	private @NonNull String password;
 	
 	@Enumerated(EnumType.STRING)
     @Column(name="role")
@@ -48,72 +49,5 @@ public class User implements Serializable {
 	
 	@Transient
     private String token;
-	
-	
-	public User() {
-	
-	}
-
-
-	public Long getId() {
-		return id;
-	}
-
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-
-	public String getName() {
-		return name;
-	}
-
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-
-	public String getUsername() {
-		return username;
-	}
-
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-
-	public String getPassword() {
-		return password;
-	}
-
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-
-	public Role getRole() {
-		return role;
-	}
-
-
-	public void setRole(Role role) {
-		this.role = role;
-	}
-
-
-	public String getToken() {
-		return token;
-	}
-
-
-	public void setToken(String token) {
-		this.token = token;
-	}
-
-
 	
 }
