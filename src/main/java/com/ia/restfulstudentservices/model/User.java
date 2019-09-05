4022cc47cbe81a67e5edf347ada6e3aff24a1790
ifofana <ibrahima.fofana.ia@gmail.com>
2019-09-05 14:40:33 -0400

    Removed the Lombak capabilities.

--- src/main/java/com/ia/restfulstudentservices/model/User.java
+++ src/main/java/com/ia/restfulstudentservices/model/User.java
@@ -11,18 +11,8 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.Setter;
-import lombok.ToString;
-
 @Entity
 @Table(name = "user")
-@Getter
-@Setter
-@ToString
-@EqualsAndHashCode
 public class User implements Serializable {
 	
 	/**
@@ -31,17 +21,17 @@ public class User implements Serializable {
 	private static final long serialVersionUID = -6147614719923616174L;
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-	private @NonNull Long id;
+	private Long id;
 	
 	
 	@Column(name = "name")
-	private @NonNull String name;
+	private String name;
 	
 	@Column(name = "username")
-	private @NonNull String username;
+	private String username;
 	
 	@Column(name = "password")
-	private @NonNull String password;
+	private String password;
 	
 	@Enumerated(EnumType.STRING)
     @Column(name="role")
@@ -50,4 +40,55 @@ public class User implements Serializable {
 	@Transient
     private String token;
 	
+    public User() {
+        
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
