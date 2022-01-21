c854ca355f10e74a515405d7da238acb570f74b2
ibrah <ifofana75@gmail.com>
2022-01-21 16:52:07 -0500

    Added @CrossOrigins url for angular code

--- src/main/java/com/ia/restfulstudentservices/controller/AdminController.java
+++ src/main/java/com/ia/restfulstudentservices/controller/AdminController.java
@@ -2,11 +2,13 @@ package com.ia.restfulstudentservices.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ia.restfulstudentservices.service.UserService;
 
+@CrossOrigin(origins = "http://angular-project-isg.s3-website-us-east-1.amazonaws.com:4200")
 @RestController
 public class AdminController {
 	
